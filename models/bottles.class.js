3f204f8100d5facd51cabb7d26b8ad866a1dfe6c
Dzmitry Stashkevich <dimarehfeldt@icloud.com>
2025-04-01 09:15:11 +0200

    bottle-animation

--- models/bottles.class.js
+++ models/bottles.class.js
@@ -14,19 +14,21 @@ class Bottle extends MovableObject {
     "img_pollo_locco/img/6_salsa_bottle/1_salsa_bottle_on_ground.png",
     "img_pollo_locco/img/6_salsa_bottle/2_salsa_bottle_on_ground.png",
   ];
+  currentImageIndex = 0;
 
   constructor() {
-    super().loadImage(
-      "img_pollo_locco/img/6_salsa_bottle/1_salsa_bottle_on_ground.png"
-    );
+    super().loadImage(this.IMAGES_BOTTLE[0]);
     this.loadImages(this.IMAGES_BOTTLE);
 
-    this.x = 100;
     this.x = 50 + Math.random() * 2100;
-    this.image = this.loadRandomImage();
+    this.startImageSwitch();
   }
-  loadRandomImage() {
-    let randomIndex = Math.floor(Math.random() * this.IMAGES_BOTTLE.length);
-    return this.loadImage(this.IMAGES_BOTTLE[randomIndex]);
+
+  startImageSwitch() {
+    setInterval(() => {
+      this.currentImageIndex =
+        (this.currentImageIndex + 1) % this.IMAGES_BOTTLE.length;
+      this.loadImage(this.IMAGES_BOTTLE[this.currentImageIndex]);
+    }, 500); // Wechselt alle 500ms das Bild
   }
 }
